2a318b6d53e382fcab133b9ebc2cbb373f1ba172
mohamed-dow <39552599+mohamed-dow@users.noreply.github.com>
2018-05-24 20:06:26 -0400

    N26 Coding challenge: initial project setup

---
+++ src/main/java/com/n26/realtime/transactions/realtimetransactionsstatisticscalculator/RealtimeTransactionsStatisticsCalculatorApplication.java
@@ -0,0 +1,12 @@
+package com.n26.realtime.transactions.realtimetransactionsstatisticscalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeTransactionsStatisticsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeTransactionsStatisticsCalculatorApplication.class, args);
+	}
+}
